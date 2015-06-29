6a0e66f16ec3813d47cd6c1f060f32a6b9572318
Jarle Hansen <jarle.hansen@gmail.com>
2015-06-29 23:08:28 +0200

    Added Csv Parser Code.

---
+++ Kraggs.TSM7.Data/Csv/CsvConvert.cs
@@ -0,0 +1,78 @@
+﻿#region License
+/*
+    TSM 7.1 Utility library.
+    Copyright (C) 2015 Jarle Hansen
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kraggs.TSM7.Data
+{
+    internal static class CsvConvert
+    {
+        public static List<T> Convert<T>(List<List<string>> CsvLines, clsTypeInfo myType, List<clsColumnInfo> Columns)
+        {
+            var result = new List<T>();
+
+            var arrColumns = Columns.ToArray();
+
+            foreach (var listValues in CsvLines)
+            {
+                var o = myType.CreateObject();
+
+                int i = 0;
+                foreach (var v in listValues)
+                {
+                    if (i == arrColumns.Length)
+                        break;
+                    clsColumnInfo column = arrColumns[i++];
+
+                    object oset = null;
+
+                    switch (column.DataType)
+                    {
+                        case TSMDataType.String:
+                            oset = v; break;
+                        case TSMDataType.Short:
+                            oset = short.Parse(v); break;
+                        case TSMDataType.Int:
+                            oset = int.Parse(v); break;
+                        case TSMDataType.Long:
+                            oset = long.Parse(v); break;
+                        case TSMDataType.Double:
+                            oset = double.Parse(v); break;
+                        case TSMDataType.Float:
+                            //oset = float.Parse(v, CultureInfo.InvariantCulture, CultureInfo.InvariantCulture )
+                            oset = float.Parse(v); break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+
+                    column.SetValue(o, oset);
+                }
+
+
+                result.Add((T)o);
+            }
+
+
+            return result;
+        }
+
+    }
+}
---
+++ Kraggs.TSM7.Data/Csv/CsvParser.cs
@@ -0,0 +1,98 @@
+﻿#region License
+/*
+    TSM 7.1 Utility library.
+    Copyright (C) 2015 Jarle Hansen
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kraggs.TSM7.Data
+{
+    internal static class CsvParser
+    {
+        public static List<string> Parse(string CsvLine, StringBuilder reuse = null, char CsvSeparator = ',')
+        {
+            if (reuse == null)
+                reuse = new StringBuilder(CsvLine.Length);
+            else
+            {
+                reuse.Length = 0;
+            }
+
+            var list = new List<string>();
+
+            var flagQ1 = false;
+            var flagQ2 = false;
+
+            foreach (char c in CsvLine.ToCharArray())
+            {
+                // fast skip inside quote[s]
+                if (
+                    (flagQ1 && c != '\'') ||
+                    (flagQ2 && c != '"'))
+                {
+                    reuse.Append(c);
+                    continue;
+                }
+                else if (c == '\'')
+                    flagQ1 = !flagQ1;
+                else if (c == '"')
+                    flagQ2 = !flagQ2;
+                else if (c == CsvSeparator)
+                {
+                    list.Add(reuse.ToString());
+                    reuse.Length = 0;
+                }
+                else
+                    reuse.Append(c);
+            }
+
+            // add list item.
+            list.Add(reuse.ToString());
+
+            return list;
+        }
+
+        public static int Parse(List<string> CsvLines, System.Collections.IList result, char CsvSeparator = ',')
+        {
+            if (CsvLines == null)
+                throw new ArgumentNullException("CsvLines");
+            if (result == null)
+                throw new ArgumentNullException("result");
+
+            var reuse = new StringBuilder();
+            var count = 0;
+
+            foreach (var line in CsvLines)
+            {
+                List<string> list = null;
+
+                list = Parse(line, reuse, CsvSeparator);
+
+                if (list != null)
+                {
+                    result.Add(list);
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
