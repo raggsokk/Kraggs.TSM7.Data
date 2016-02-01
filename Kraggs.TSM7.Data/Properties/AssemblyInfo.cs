b321c71dd6bed1f4e807d4b1c991192f2c61cbca
Jarle Hansen <jarle.hansen@gmail.com>
2016-02-01 22:06:04 +0100

    Bumped minor version. Build against Kraggs.TSM7.Utils v0.4

--- Kraggs.TSM7.Data/Properties/AssemblyInfo.cs
+++ Kraggs.TSM7.Data/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.5.*")]
 //[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.2.0")]
